dcfa754266b6f5afa5faa6dcc2acadbf02d38cac
chloetedder <chloe.tedder@vercel.com>
2023-10-03 10:59:51 -0600

    add config

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -12,3 +12,9 @@ export default async function handler(req, res) {
       res.status(200).json({ message: 'Welcome to API Routes!'})
   }
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
